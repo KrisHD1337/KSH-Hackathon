136dba52730f564e40f56caea986910da812ba42
KrisHD1337 <kriscerni@gmail.com>
2025-08-23 10:09:19 +0200

    Added route for kgv/ggt

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from math import lcm, gcd
 from functools import reduce
 from flask import Flask, render_template, request, url_for, redirect, send_file, session, current_app, flash
 app = Flask(__name__)
+@app.route("/kopfrechnen")
+def kopfrechnen():
+    def kgv(nums):
+        return reduce(lcm, nums)
 
-def kgv(nums):
-    return reduce(lcm, nums)
-
-def ggt(nums):
-    return reduce(gcd, nums)
+    def ggt(nums):
+        return reduce(gcd, nums)
 
 @app.route("/")
 def home():
